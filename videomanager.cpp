8c532cd9769806ee9787ec75e7d697066d516863
Derek Spaulding <derek.spaulding@snhu.edu>
2025-05-06 04:13:08 -0500

    Initial Setup of VideoManager

--- videomanager.cpp
+++ videomanager.cpp
@@ -17,7 +17,7 @@ VideoManager::VideoManager(const char* input_file, const char* output_file) {
     this->video_stream_idx = -1;
     this->audio_stream_idx = -1;
     this->options = nullptr;
-    this->audio_codec_ctx = nullptr;
+    this->input_audio_codec_ctx = nullptr;
 }
 
 
@@ -26,20 +26,18 @@ VideoManager::~VideoManager() {
 }
 
 
-bool VideoManager::validateFileExists() {
-
+int VideoManager::validateFileExists() {
+    if (avformat_open_input(&this->format_ctx, this->input_file, nullptr, nullptr) < 0) {
+        std::cerr << "Could not open input file" << std::endl;
+        return 1;
+    }
 }
 
 
-bool VideoManager::setOptions() {
-    
-}
-
-
-int VideoManager::populateFormatContext(AVFormatContext* format_ctx, const char* input_file) {
+int VideoManager::populateFormatContext() {
     // Open the input file and populate the format context with file information
-    if (avformat_open_input(&format_ctx, input_file, nullptr, nullptr) < 0) {
-        std::cerr << "Could not open input file: " << input_file << std::endl;
+    if (avformat_open_input(&this->format_ctx, this->input_file, nullptr, nullptr) < 0) {
+        std::cerr << "Could not open input file: " << this->input_file << std::endl;
         return 0;  // Return error code if file couldn't be opened
     }
 
@@ -53,9 +51,9 @@ int VideoManager::populateFormatContext(AVFormatContext* format_ctx, const char*
 }
 
 
-int VideoManager::getAudioStream(const char* input_file) {
-    for (unsigned int i = 0; i < this->input_format_ctx->nb_streams; i++) {
-        if (this->input_format_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+int VideoManager::getAudioStream() {
+    for (unsigned int i = 0; i < this->format_ctx->nb_streams; i++) {
+        if (this->format_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
             this->audio_stream_idx = i;
             break;
         }
@@ -71,7 +69,7 @@ int VideoManager::getAudioStream(const char* input_file) {
 
 
 const AVCodec* VideoManager::getAudioCodec() {
-    const AVCodec* audio_codec = avcodec_find_decoder(this->input_format_ctx->streams[this->audio_stream_idx]->codecpar->codec_id);
+    const AVCodec* audio_codec = avcodec_find_decoder(this->format_ctx->streams[this->audio_stream_idx]->codecpar->codec_id);
     if (!audio_codec) {
         std::cerr << "Could not find audio codec" << std::endl;
         return nullptr;
@@ -83,8 +81,8 @@ const AVCodec* VideoManager::getAudioCodec() {
 
 int VideoManager::setAudioCodec() {
     // Allocate codec context
-    this->audio_codec_ctx = avcodec_alloc_context3(this->audio_codec);
-    if (!this->audio_codec_ctx) {
+    this->input_audio_codec_ctx = avcodec_alloc_context3(this->getAudioCodec());
+    if (!this->input_audio_codec_ctx) {
         std::cerr << "Could not allocate audio codec context" << std::endl;
         return 0;
     }
@@ -95,7 +93,7 @@ int VideoManager::setAudioCodec() {
 
 int VideoManager::copyAudioCodecParams() {
     // Copy codec parameters based on input format
-    if (avcodec_parameters_to_context(this->audio_codec_ctx, this->input_format_ctx->streams[this->audio_stream_idx]->codecpar) < 0) {
+    if (avcodec_parameters_to_context(this->input_audio_codec_ctx, this->format_ctx->streams[this->audio_stream_idx]->codecpar) < 0) {
         std::cerr << "Could not copy codec parameters" << std::endl;
         return 0;
     }
@@ -105,7 +103,7 @@ int VideoManager::copyAudioCodecParams() {
 
 
 int VideoManager::openAudioCodec() {
-    if (avcodec_open2(this->audio_codec_ctx, this->audio_codec, nullptr) < 0) {
+    if (avcodec_open2(this->input_audio_codec_ctx, this->getAudioCodec(), nullptr) < 0) {
         std::cerr << "Could not open audio codec" << std::endl;
         return 0;
     }
@@ -115,7 +113,7 @@ int VideoManager::openAudioCodec() {
 
 
 int VideoManager::createOutputContext() {
-    if (avformat_alloc_output_context2(&this->output_format_ctx, nullptr, nullptr, this->output_file) < 0) {
+    if (avformat_alloc_output_context2(&this->output_ctx, nullptr, nullptr, this->output_file) < 0) {
         std::cerr << "Could not create output context" << std::endl;
         return 0;
     }
--- videomanager.h
+++ videomanager.h
@@ -12,11 +12,20 @@ private:
     int video_stream_idx;
     int audio_stream_idx;
     AVDictionary* options;
-    AVCodecContext* audio_codec_ctx;
+    AVCodecContext* input_audio_codec_ctx;
+    AVCodecContext* output_audio_format_ctx;
 
 public:
-    VideoManager();
+    VideoManager(const char* input_file, const char* output_file);
     ~VideoManager();
+    int validateFileExists();
+    int populateFormatContext();
+    int getAudioStream();
+    const AVCodec* getAudioCodec();
+    int setAudioCodec();
+    int copyAudioCodecParams();
+    int openAudioCodec();
+    int createOutputContext();
 };
 
 #endif
